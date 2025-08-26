3d6563592df1e4c9322cca3bad153cb83664f6ca
園部真生 <s2522023@stu.musashino-u.ac.jp>
2025-08-26 23:16:45 +0900

    ローカル開発環境の修正

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 3000,
+    open: true
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
